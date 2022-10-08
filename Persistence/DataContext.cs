6a9d9acad9705dc5e6bcba7668bae887d4e521ea
sattep91 <paul.webdevguy@gmail.com>
2022-10-08 13:41:44 -0500

    new Post class created

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +10,7 @@ namespace Persistence
   public class DataContext: DbContext
   {
     public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    public DbSet<Post> Posts { get; set; }
     public string DbPath { get; }
 
     public DataContext()
